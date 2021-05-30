7432396d6104441e37963a9f7a91d275a6957f9c
metaljefferson <metaljefferson11@hotmail.com>
2021-05-30 15:18:49 -0300

    template renderizado atraves do JSON

---
+++ data.js
@@ -0,0 +1,39 @@
+const livros = [{
+    "titulo_livro": "mitologia nordica",
+    "status_reserva": "disponivel",
+    "texto_livro": "Quem, além de Neil Gaiman, poderia se tornar cúmplice dos deuses e usar de sua habilidade com as palavras. Este conteúdo é um pouco mais longo.",
+    "capa": "mitologia nordica.jpg",
+    "ano_lancamento": 2014,
+}, {
+    "titulo_livro": "o homem de giz",
+    "status_reserva": "disponivel",
+    "texto_livro": "Assassinato e sinais misteriosos em uma trama para fãs de Stranger Things e Stephen King Em 1986, Eddie e os amigos passam a maior parte dos dias.",
+    "capa": "o homem de giz.png",
+    "ano_lancamento": 2001,
+}, {
+    "titulo_livro": "A Garota do Lago",
+    "status_reserva": "disponivel",
+    "texto_livro": "Summit Lake, uma pequena cidade entre montanhas, é esse tipo de lugar, bucólico e com encantadoras casas dispostas à beira de um longo trecho.",
+    "capa": "a garota do lago.png",
+    "ano_lancamento": 2005,
+}, {
+    "titulo_livro": "o que aconteceu com annie",
+    "status_reserva": "indisponivel",
+    "texto_livro": "Quando Joe Thorne era adolescente, sua irma mais nova desapareceu. Vinte e cinco anos depois, um e-mail anonimo o leva mais uma vez ao passado.",
+    "capa": "o que aconteceu com annie.png",
+    "ano_lancamento": 2002,
+}, {
+    "titulo_livro": "it a coisa",
+    "status_reserva": "indisponivel",
+    "texto_livro": "Durante as férias escolares de 1958, em Derry, pacata cidadezinha do Maine, Bill, Richie, Stan, Mike, Eddie, Ben e Beverly aprenderam o real sentido da amizade, do amor,.",
+    "capa": "it a coisa.png",
+    "ano_lancamento": 2011,
+
+}, {
+    "titulo_livro": "pai rico pai pobre",
+    "status_reserva": "indisponivel",
+    "texto_livro": "Celebrando 20 anos como o livro n° 1 em finanças pessoais. A escola prepara as crianças para o mundo real.Essa é a primeira pergunta com a qual o leitor se depara neste livro",
+    "capa": "pai rico pai pobre.png",
+    "ano_lancamento": 2007,
+
+}]
